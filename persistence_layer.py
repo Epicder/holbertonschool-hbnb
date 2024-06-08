2f4991996a9b57b9af90769b835a20e29afb0b8c
Joaquin <aguilarj4126@gmail.com>
2024-06-08 01:24:06 -0300

    add DataManager subclass prototipe

--- persistence_layer.py
+++ persistence_layer.py
@@ -1,20 +1,62 @@
 #!/usr/bin/python3
 
 from abc import ABC, abstractmethod
+from business_logic_layer import Users, Reviews, Place, Amenities
 
 class IPersistenceManager(ABC):
        @abstractmethod
-       def save(self, entity):
+       def save(self, instance):
            pass
 
        @abstractmethod
-       def get(self, entity_id, entity_type):
+       def get(self, instance_id, instance_type):
            pass
 
        @abstractmethod
-       def update(self, entity):
+       def update(self, instance):
            pass
 
        @abstractmethod
-       def delete(self, entity_id, entity_type):
+       def delete(self, instance_id, instance_type):
            pass
+       
+class DataManager(IPersistenceManager):
+    def __init__(self):
+        self.data_lists = {
+        Users: [],
+        Reviews: [],
+        Place : [],
+        Amenities:[]
+        }
+        
+    def save(self, instance):
+        instance_type = type(instance)
+        if instance_type in self.data_lists:
+            self.data_lists[instance_type].append(instance)
+        else:
+            print(f"Invalid Object: {instance_type}")
+   
+    def get(self, instance_id, instance_type):
+        if instance_type in self.data_lists:
+            instance_list = self.data_lists[instance_type]
+            for instance in instance_list:
+                if instance.id == instance_id:
+                    return instance
+
+            print(f"Invalid id: {instance_id}")
+        else:
+            print(f"Invalid Object: {instance_type}")
+        
+    
+    def delete(self, instance_id, instance_type):
+        if instance_type in self.data_lists:
+            instance_list = self.data_lists[instance_type]
+            for instance in instance_list:
+                if instance.id == instance_id:
+                    self.data_lists[instance_type].remove(instance)
+        else:
+            print(f"Invalid Objetct: {instance_type}")
+        
+    
+    def update(self, instance):
+       pass
