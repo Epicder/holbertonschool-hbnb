3753fd5e76b15a199d90da555bcc6f7df75e2185
nachoneca <159149484+nachoneca@users.noreply.github.com>
2024-06-08 01:51:36 -0300

    Fixing inicialization of super class, fixed when created an instance of review and place

--- business_logic_layer.py
+++ business_logic_layer.py
@@ -12,15 +12,15 @@ class Basic_data(ABC):
 
 class Users(Basic_data):
     def __init__(self, id, created_at, updated_at, email, password, first_name, last_name):
-        super.__init__(id, created_at, updated_at)
+        super().__init__()
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
-                
+
 class Reviews(Basic_data):
     def __init__(self, id, created_at, updated_at, rating, user_id, place_id, comment):
-        super.__init__(id, created_at, updated_at)
+        super().__init__()
         self.rating = rating
         self.user_id = user_id
         self.place_id = place_id
@@ -29,7 +29,7 @@ class Reviews(Basic_data):
 class Place(Basic_data):
     def __init__(self, id, created_at, updated_at, host_id, name, description, rooms, bathrooms,\
                 max_guests, price_per_night, latitude, longitude, city_id, amenities_list):
-        super.__init__(id, created_at, updated_at)
+        super().__init__()
         self.host_id = host_id
         self.name = name
         self.description = description
@@ -45,11 +45,11 @@ class Place(Basic_data):
 class Country:
     def __init__(self, code, name):
         self.code = code
-        self.code = name
+        self.name = name
 
 class City(Basic_data):
     def __init__(self, id, created_at, updated_at, name, country_code):
-        super.__init__(id, created_at, updated_at)
+        super().__init__()
         self.name = name
         self.county_code = country_code
 
@@ -59,77 +59,75 @@ class Amenities:
         self.name = name
 
 class System:
-    
+
     def create_review(data_reviwe):
-        new_review = Reviews()
-        new_review.user_id = data_reviwe.get('user_id')
-        new_review.place_id = data_reviwe.get('place_id')
-        new_review.comment = data_reviwe.get('comment')
         try:
-            new_review.rating = data_reviwe.get('rating')
+            new_review = Reviews(
+                new_review.user_id = data_reviwe.get('user_id')
+                new_review.place_id = data_reviwe.get('place_id')
+                new_review.comment = data_reviwe.get('comment')
+                new_review.rating = data_reviwe.get('rating')
+            )
+            if new_review.rating <= 0:
+                raise ValueError("Rating must be a number from 1 to 5")
+            if new_review.comment == "":
+                raise TypeError("A comment must be written")
             print("Thank you for your review!")
         except Exception:
-            print("Please enter a integer between 1 and 5")
-        
+            print("An error has occured, please try again!")
         return new_review
-    
+
     def create_place(data_place):
-        
-        new_plance = Place()
-        new_plance.name = data_place.get('name')
-        new_plance.host_id = data_place.get('host_id')
-        new_plance.description = data_place.get('description')
+
         try:
-            new_plance.rooms = data_place.get('rooms')
-        except Exception:
-                print("Please enter a valid number")
-        try:
-            new_plance.bathroom = int(data_place.get('bathrooms'))    
-        except Exception:
-                print("Please enter a valid number")
-        try:
-             new_plance.max_guests = int(data_place.get('max_guests'))  
-        except Exception:
-                print("Please enter a valid number")
-        try:
-             new_plance.price_per_night =  float(data_place.get('precie_per_nigth'))
-        except Exception:
-                print("Please enter a valid number")
-        try:
-            new_plance.latitude = float(data_place.get('latitude'))
+            new_plance = Place(
+                new_plance.name = data_place.get('name')
+                new_plance.host_id = data_place.get('host_id')
+                new_plance.description = data_place.get('description')
+                new_plance.rooms = data_place.get('rooms')
+                new_plance.bathroom = int(data_place.get('bathrooms'))
+                new_plance.max_guests = int(data_place.get('max_guests'))
+                new_plance.price_per_night =  float(data_place.get('precie_per_nigth'))
+                new_plance.latitude = float(data_place.get('latitude'))
+                new_plance.longitude = float(data_place.get('longitude'))
+                new_plance.city_id = data_place.get('city_id')
+                new_plance.amenities_list = data_place.get('amenities_list')
+                )
+            if new_plance.description == "":
+                raise TypeError("The place must have a description!")
+            if new_plance.rooms <= 0:
+                raise ValueError("The place must have at least one room!")
+            if new_plance.bathroom <= 0:
+                raise ValueError("The place must have at least one bathroom!")
+            if new_plance.max_guests <= 0:
+                raise ValueError("The place must have at least one guest!")
+            if new_plance.price_per_night <= 0:
+                raise ValueError("Price per night must be positive!")
             if not -90 <= new_plance.latitude <= 90:
                 raise ValueError("Please enter a latitud between -90 and 90")
-        except ValueError:
-                print("Please enter a latitud between -90 and 90")
-        try:
-                new_plance.longitude = float(data_place.get('longitude'))
-                if not -180 <= new_plance.longitude <= 180:
-                    raise ValueError("Please enter a longitude between -180 and 180")   
-        except ValueError:
-                print("Please enter a longitude between -180 and 180")
-        
-        new_plance.city_id = data_place.get('city_id')
-        new_plance.amenities_list = data_place.get('amenities_list')
-        
-        return new_plance
-    
+            if not -180 <= new_plance.longitude <= 180:
+                    raise ValueError("Please enter a longitude between -180 and 180")
+            except Exception:
+                print("Error creating a place, please try again!")
+            return new_plance
+
     def create_amenities(data_amenities):
         new_amenities = Amenities()
         new_amenities.id = data_amenities.get('id')
         new_amenities = data_amenities.get('name')
-        
+
         return new_amenities
-    
+
     def create_user(data_user):
         new_user = Users()
         new_user.email = data_user.get('email')
         new_user.password = data_user.get('password')
         new_user.first_name = data_user.get('first_name')
         new_user.last_name = data_user.get('last_name')
-        
+
         return new_user
-        
-        
+
+
     """
     def create_review(user_id, place_id):
         new_review = Reviews()
@@ -193,6 +191,7 @@ class System:
                     break
             except ValueError:
                 print("Please enter a longitude between -180 and 180")
-    
+
         pass  ## Falta Agregar el sistema de amenities, el que las crea y luego como agregarlas aca.
     """
+~                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ~                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ~                                                                                                                                     
