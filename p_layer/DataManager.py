55f6622fdbaf7ff736e2e78a5d40ff2a5811da5d
Joaquin <aguilarj4126@gmail.com>
2024-06-12 13:22:54 -0300

    add persistence in a file and fix delete and update method

--- p_layer/DataManager.py
+++ p_layer/DataManager.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from models.IPersistenceManager import IPersistenceManager
+import json
 
 class DataManager(IPersistenceManager):
     def __init__(self):
@@ -12,11 +13,23 @@ class DataManager(IPersistenceManager):
         'Country': [],
         'City': []
         }
-        
+        try:
+            with open('data_base.json', encoding="utf-8") as file:
+                self.data_lists = json.load(file)
+        except FileNotFoundError:
+            with open('data_base.json', encoding="utf-8") as file:
+                file.write(json.dumps(self.data_lists))
+
     def save(self, instance):
         instance_name = instance.__class__.__name__
         if instance_name in self.data_lists:
             self.data_lists[instance_name].append(instance.__dict__)
+        try:
+            with open('data_base.json', encoding="utf-8") as file:
+                file.write(json.dumps(self.data_lists))
+        except FileNotFoundError:
+            return ("File not found"), 404
+
         else:
             print(f"Invalid Object: {instance_name}")
    
@@ -44,7 +57,7 @@ class DataManager(IPersistenceManager):
         if instance_name in self.data_lists:
             instance_list = self.data_lists[instance_name]
             for instance in instance_list:
-                if instance.id == instance_id:
+                if instance.get('get') == instance_id:
                     self.data_lists[instance_name].remove(instance)
         else:
             print(f"Invalid Objetct: {instance_name}")
@@ -55,9 +68,14 @@ class DataManager(IPersistenceManager):
        if instance_name in self.data_lists:
            instance_list = self.data_lists[instance_name]
            for obj_id in instance_list:
-               if obj_id.id == instance.id:
+               if obj_id.get('id') == instance.get('id'):
                    instance_list[obj_id] = instance
                    self.data_lists[instance_name] = instance_list
-                   break
+                   try:
+                        with open('data_base.json', encoding="utf-8") as file:
+                            file.write(json.dumps(self.data_lists))
+                            return ("File update"), 200
+                   except FileNotFoundError:
+                       return ("File not found"), 404
        else:
             print(f"Invalid Objetct: {instance_name}")
