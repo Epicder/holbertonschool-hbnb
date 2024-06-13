5ba87d2e8d5c48eb043ea81c755d8ae4dfe559a8
Joaquin <aguilarj4126@gmail.com>
2024-06-13 10:51:24 -0300

    fix init in basic_data and some changes in data manager

--- models/basic_data.py
+++ models/basic_data.py
@@ -7,5 +7,6 @@ from datetime import datetime
 class Basic_data(ABC):
     def __init__(self):
         self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.id = str(self.id)
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
